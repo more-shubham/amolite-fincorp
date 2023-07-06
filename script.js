455ad96b06b442a79018cc2a643fca45b7a3eb3d
Shubham More <mores9785@gmail.com>
2023-07-06 21:03:35 +0530

    update

---
+++ script.js
@@ -0,0 +1,22 @@
+// $('.autoplay').slick({
+//     slidesToShow: 3,
+//     slidesToScroll: 1,
+//     autoplay: true,
+//     autoplaySpeed: 2000,
+// });
+
+// $(document).ready(function(){
+//     $('.your-class').slick({
+//       setting-name: setting-value
+//     });
+//   });
+  
+$(document).ready(function(){
+    $('.autoplay').slick({
+        slidesToShow: 3,
+        dots: true,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+    })
+})

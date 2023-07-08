cee4aebcf926e07195799b5bea66cf6dda5edf6d
Shubham More <mores9785@gmail.com>
2023-07-08 20:19:00 +0530

    update

--- script.js
+++ script.js
@@ -1,26 +1,45 @@
+// $(document).ready(function () {
+//     $(".regular").slick({
+//         dots: true,
+//         infinite: true,
+//         slidesToShow: 3,
+//         adaptiveHeight: true,
+//         infinite: false,
+//         slidesToScroll: 1,
+//         zIndex: 0,
+//         responsive: [
+//             {
+//               breakpoint: 1024,
+//               settings: {
+//                 dots: true,
+//                 infinite: true,
+//                 slidesToShow: 1,
+//                 adaptiveHeight: true,
+//                 infinite: false,
+//                 zIndex: 0,
+//                 slidesToScroll: 1,
+//               }
+//             }
+//           ]
+
+//     });
+// })
+
 $(document).ready(function () {
-    $(".regular").slick({
-        dots: true,
-        infinite: true,
-        slidesToShow: 3,
-        adaptiveHeight: true,
-        infinite: false,
-        slidesToScroll: 1,
-        zIndex: 0,
-        responsive: [
-            {
-              breakpoint: 1024,
-              settings: {
-                dots: true,
-                infinite: true,
-                slidesToShow: 1,
-                adaptiveHeight: true,
-                infinite: false,
-                zIndex: 0,
-                slidesToScroll: 1,
-              }
-            }
-          ]
-        
-    });
-})
+  $(".owl-carousel").owlCarousel({
+    loop: true,
+    margin: 10,
+    autoplay:true,
+    autoplayTimeout:5000,
+    autoplayHoverPause:true,
+    // nav: true,
+    responsive: {
+      0: {
+        items: 1
+      },
+      992: {
+        items: 3
+      }
+    }
+  });
+});

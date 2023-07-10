8cbdc40f2ef6a68c4fb52c571a38d190b34ee87a
Shubham More <mores9785@gmail.com>
2023-07-10 23:48:10 +0530

    update

--- script.js
+++ script.js
@@ -1,37 +1,10 @@
-// $(document).ready(function () {
-//     $(".regular").slick({
-//         dots: true,
-//         infinite: true,
-//         slidesToShow: 3,
-//         adaptiveHeight: true,
-//         infinite: false,
-//         slidesToScroll: 1,
-//         zIndex: 0,
-//         responsive: [
-//             {
-//               breakpoint: 1024,
-//               settings: {
-//                 dots: true,
-//                 infinite: true,
-//                 slidesToShow: 1,
-//                 adaptiveHeight: true,
-//                 infinite: false,
-//                 zIndex: 0,
-//                 slidesToScroll: 1,
-//               }
-//             }
-//           ]
-
-//     });
-// })
-
 $(document).ready(function () {
   $(".owl-carousel").owlCarousel({
     loop: true,
     margin: 10,
     autoplay: true,
     autoplayTimeout: 5000,
-    autoplayHoverPause: true,
+    // autoplayHoverPause: true,
     // nav: true,
     responsive: {
       0: {
@@ -45,12 +18,5 @@ $(document).ready(function () {
 
 
   $('body').scrollspy({ target: '#navbar' });
-
-
-  // $('#navbar').on('activate.bs.scrollspy', function () {
-  //   var activeSection = $('#navbar .nav-item').attr('href');
-  //   $('#navbar .nav-item').removeClass('custom-active');
-  //   $(activeSection).addClass('custom-active');
-  // });
 
 });
